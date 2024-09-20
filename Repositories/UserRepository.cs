b3554866204a0bfacb53e8079e1cc6f81aee1b4a
marinacanal <eromakcanal@gmail.com>
2024-09-19 22:49:50 -0300

    getuserbyname method added

--- Repositories/UserRepository.cs
+++ Repositories/UserRepository.cs
@@ -6,9 +6,15 @@ namespace barbieProject.Repositories
     public class UserRepository : Repository<User>
     {
         private readonly DbContext _context;
+        private readonly DbSet<User> _dbSet;
         public UserRepository(DbContext context) : base(context)
         {
             _context = context;
+            _dbSet = _context.Set<User>();
+        }
+
+        public async Task<User> GetUserByNameAsync(string username) {
+            return await _dbSet.FirstOrDefaultAsync(user => user.UserName == username);
         }
     }
 }
