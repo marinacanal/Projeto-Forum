6e8a2edc1dddc2b6b1fa792e456f995e44034be9
Marina Canal <eromakcanal@gmail.com>
2024-11-23 19:28:43 -0300

    deleted wrong folder

--- Forum/Domain/User/Services/IPasswordHasher.cs
+++
@@ -1,8 +0,0 @@
-namespace Domain.User.Service
-{
-    public interface IPasswordHasher
-    {
-        string HashPassword(string password);
-        bool VerifyHashedPassword(string salt_hashedPassword, string password);
-    }
-}
