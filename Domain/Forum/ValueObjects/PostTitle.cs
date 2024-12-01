090a65b6c286a0d252ceee82c0a41eddce04497b
Marina Canal <eromakcanal@gmail.com>
2024-12-01 20:09:56 -0300

    correcting assembly references

--- Domain/Forum/ValueObjects/PostTitle.cs
+++ Domain/Forum/ValueObjects/PostTitle.cs
@@ -1,9 +1,9 @@
 namespace Domain.Forum.ValueObjects
 {   
-    public class Title
+    public class PostTitle
     {
         public string Value { get; private set; }
 
-        public Title(string value) => Value = value ?? throw new ArgumentNullException("Título não pode ser vazio!");
+        public PostTitle(string value) => Value = value ?? throw new ArgumentNullException("Título não pode ser vazio!");
     }
 }
