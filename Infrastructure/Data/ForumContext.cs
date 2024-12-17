80415f99f552dcb6dcecaaa765f1e299671709cf
Marina Canal <eromakcanal@gmail.com>
2024-12-16 21:49:44 -0300

    update context

--- Infrastructure/Data/ForumContext.cs
+++ Infrastructure/Data/ForumContext.cs
@@ -1,4 +1,4 @@
-using Forum.Models;
+using Domain.Forum.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Context
@@ -8,7 +8,7 @@ namespace Infrastructure.Context
         public ForumContext(DbContextOptions<ForumContext> options) : base(options) { }
         public DbSet<User> Users { get; set; }
         public DbSet<Post> Posts { get; set; }
-        public DbSet<Like> Likes { get; set; }
+        public DbSet<Reaction> Reactions { get; set; }
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Channel> Channels { get; set; }
         public DbSet<ChannelMembers> ChannelMembers { get; set; }
